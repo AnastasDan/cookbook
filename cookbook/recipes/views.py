69500a4ef904e3c05282ae44fdf01559dd703a29
AnastasDan <danielyan.anastas@gmail.com>
2024-02-04 16:19:12 +0200

    Поправил код

--- cookbook/recipes/views.py
+++ cookbook/recipes/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models import F
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404, render
@@ -5,65 +6,65 @@ from django.shortcuts import get_object_or_404, render
 from .models import Product, Recipe, RecipeProduct
 
 
+@transaction.atomic
 def add_product_to_recipe(request):
     """Добавляет продукт к рецепту или обновляет его вес."""
-    recipe_id = request.GET.get("recipe_id")
-    product_id = request.GET.get("product_id")
-    weight = request.GET.get("weight")
-
-    if not all([recipe_id, product_id, weight]):
-        return HttpResponseBadRequest("Недостаточно параметров")
-
     try:
-        weight = int(weight)
-    except ValueError:
-        return HttpResponseBadRequest("Некорректный формат веса")
+        recipe_id = request.GET.get("recipe_id")
+        product_id = request.GET.get("product_id")
+        weight = int(request.GET.get("weight"))
+        recipe = get_object_or_404(Recipe, pk=recipe_id)
+        product = get_object_or_404(Product, pk=product_id)
 
-    recipe = get_object_or_404(Recipe, pk=recipe_id)
-    product = get_object_or_404(Product, pk=product_id)
-
-    recipe_product = RecipeProduct.objects.filter(
-        recipe=recipe, product=product
-    ).first()
-
-    if recipe_product:
-        recipe_product.weight = weight
-        recipe_product.save()
-        action = "обновлен в рецепте"
-    else:
-        recipe_product = RecipeProduct.objects.create(
-            recipe=recipe, product=product, weight=weight
+        recipe_product, created = RecipeProduct.objects.update_or_create(
+            recipe=recipe, product=product, defaults={"weight": weight}
         )
-        action = "добавлен в рецепт"
 
-    return HttpResponse(f"Продукт '{product.name}' {action} '{recipe.name}'")
+        return HttpResponse(
+            f"Продукт '{product.name}' "
+            f"{'добавлен в рецепт' if created else 'обновлен в рецепте'} "
+            f"'{recipe.name}'"
+        )
+
+    except ValueError as ve:
+        return HttpResponseBadRequest(f"Некорректный формат: {ve}")
+
+    except Exception as e:
+        return HttpResponseBadRequest(f"Ошибка: {e}")
 
 
+@transaction.atomic
 def cook_recipe(request):
     """Готовит рецепт, увеличивая счетчик использования продуктов."""
-    recipe_id = request.GET.get("recipe_id")
+    try:
+        recipe_id = request.GET.get("recipe_id")
+        recipe = get_object_or_404(Recipe, pk=recipe_id)
 
-    if not recipe_id:
-        return HttpResponseBadRequest("Недостаточно параметров")
+        recipe.products.all().update(times_used=F("times_used") + 1)
+        return HttpResponse(f"Рецепт '{recipe.name}' приготовлен")
 
-    recipe = get_object_or_404(Recipe, pk=recipe_id)
-    recipe.products.all().update(times_used=F("times_used") + 1)
+    except ValueError as ve:
+        return HttpResponseBadRequest(f"Некорректный формат: {ve}")
 
-    return HttpResponse(f"Рецепт '{recipe.name}' приготовлен")
+    except Exception as e:
+        return HttpResponseBadRequest(f"Ошибка: {e}")
 
 
 def show_recipes_without_product(request):
     """ "Отображает рецепты, где продукт отсутствует или его вес менее 10 г."""
-    product_id = request.GET.get("product_id")
+    try:
+        product_id = request.GET.get("product_id")
+        product = get_object_or_404(Product, pk=product_id)
 
-    if not product_id:
-        return HttpResponseBadRequest("Недостаточно параметров")
+        recipes = Recipe.objects.exclude(
+            recipeproduct__product=product, recipeproduct__weight__gte=10
+        ).distinct()
 
-    product = get_object_or_404(Product, pk=product_id)
+        context = {"recipes": recipes, "product": product}
+        return render(request, "recipes.html", context)
 
-    recipes = Recipe.objects.exclude(
-        recipeproduct__product=product, recipeproduct__weight__gte=10
-    )
+    except ValueError as ve:
+        return HttpResponseBadRequest(f"Некорректный формат: {ve}")
 
-    context = {"recipes": recipes, "product": product}
-    return render(request, "recipes.html", context)
+    except Exception as e:
+        return HttpResponseBadRequest(f"Ошибка: {e}")
